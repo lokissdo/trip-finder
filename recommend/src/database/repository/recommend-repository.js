0f4559b480f6b39d9734eb26089ca6dc66e378a9
hung <dokhaihung2003@gmai.com>
2024-05-07 02:04:13 +0700

    modify getMiddlePoint func

--- recommend/src/database/repository/recommend-repository.js
+++ recommend/src/database/repository/recommend-repository.js
@@ -22,26 +22,26 @@ class RecommendRepository {
         // Get the number of days between the start and end date
         const numOfDays = Math.floor((endDate - startDate) / (1000 * 60 * 60 * 24));
 
-        var hotel = null,  dailySchedules = [];
+        var hotel = null, dailySchedules = [];
 
         if (!costOptions.itinerary) {
             throw new Error("Itinerary cost option is required");
         }
-         dailySchedules = await this.dailyScheduleRepository.createLandscapeScheduleByDays(province, numOfDays, costOptions.itinerary, userOptions.cheapestItinerary? true: false);
+        dailySchedules = await this.dailyScheduleRepository.createLandscapeScheduleByDays(province, numOfDays, costOptions.itinerary, userOptions.cheapestItinerary ? true : false);
 
         if (dailySchedules === -1) {
             throw new Error("Not enough suitable landscapes in the database");
         }
 
         const { lat, long } = await this.getMiddlePointOfItinerary(dailySchedules);
-        
+
         if (costOptions.hotel) {
             const hotelRequest = {
                 event: 'GET_RANDOM_HOTEL_BY_PARAMETERS',
                 data: {
-                    startPrice: costOptions.hotel*(1 - COST_HOTEL_GAP_RATE),
-                    endPrice: costOptions.hotel*(1 + COST_HOTEL_GAP_RATE),
-                    bestPrice : userOptions.cheapestHotel ? true: false,
+                    startPrice: costOptions.hotel * (1 - COST_HOTEL_GAP_RATE),
+                    endPrice: costOptions.hotel * (1 + COST_HOTEL_GAP_RATE),
+                    bestPrice: userOptions.cheapestHotel ? true : false,
                     checkinDate: startDate,
                     province: province,
                     location: {
@@ -64,9 +64,9 @@ class RecommendRepository {
                     type: vehicleType,
                     startDate: startDate,
                     endDate: endDate,
-                    startPrice: costOptions.vehicle*(1 - COST_VEHICLE_GAP_RATE),
-                    endPrice: costOptions.vehicle*(1 + COST_VEHICLE_GAP_RATE),
-                    bestPrice : userOptions.cheapestVehicle ? true: false,
+                    startPrice: costOptions.vehicle * (1 - COST_VEHICLE_GAP_RATE),
+                    endPrice: costOptions.vehicle * (1 + COST_VEHICLE_GAP_RATE),
+                    bestPrice: userOptions.cheapestVehicle ? true : false,
                 }
             };
             const vehicleResponse = await RPCRequest(SEARCH_SERVICE, vehicleRequest);
@@ -75,16 +75,16 @@ class RecommendRepository {
 
 
 
-        if (costOptions.restaurant){
-            let maxPriceQuery = costOptions.restaurant*(1+COST_RESTAURANT_GAP_RATE) / (2*numOfDays) ; // for midday and afternoon
+        if (costOptions.restaurant) {
+            let maxPriceQuery = costOptions.restaurant * (1 + COST_RESTAURANT_GAP_RATE) / (2 * numOfDays); // for midday and afternoon
             for (let i = 0; i < dailySchedules.length; i++) {
                 const schedule = dailySchedules[i];
                 const restaurantRequest = {
                     event: 'GET_RANDOM_RESTAURANT_BY_PARAMETERS',
                     data: {
-                        startPrice: maxPriceQuery*(1 - COST_RESTAURANT_GAP_RATE),
-                        endPrice: maxPriceQuery*(1 + COST_RESTAURANT_GAP_RATE),
-                        bestPrice : userOptions.cheapestRestaurant ? true: false,
+                        startPrice: maxPriceQuery * (1 - COST_RESTAURANT_GAP_RATE),
+                        endPrice: maxPriceQuery * (1 + COST_RESTAURANT_GAP_RATE),
+                        bestPrice: userOptions.cheapestRestaurant ? true : false,
                         province: province,
                         location: {
                             lat: schedule.morning.lat,
@@ -103,15 +103,15 @@ class RecommendRepository {
                 schedule.afternoonRestaurant = afternoonRestaurantResponse.data;
 
                 dailySchedules[i] = schedule;
-                
+
             }
-           
+
         }
 
         let weatherData = await this.weatherRepository.getWeatherByProvince(province, startDate);
 
         return {
-            weather : weatherData, 
+            weather: weatherData,
             hotel,
             vehicles,
             dailySchedules
@@ -124,8 +124,16 @@ class RecommendRepository {
         let lat = 0;
         let long = 0;
         dailySchedules.forEach(schedule => {
-            lat += schedule.morning.lat + schedule.afternoon.lat + (schedule.evening? schedule.evening.lat : 0);
-            long += schedule.morning.long + schedule.afternoon.long + (schedule.evening? schedule.evening.long : 0);
+            if (schedule.evening) {
+                lat += (schedule.morning.lat + schedule.afternoon.lat + schedule.evening.lat) / 3
+                long += (schedule.morning.long + schedule.afternoon.long + schedule.evening.long) / 3
+            }
+            else {
+                lat += (schedule.morning.lat + schedule.afternoon.lat) / 2
+                long += (schedule.morning.long + schedule.afternoon.long) / 2
+            }
+
+
         });
         return {
             lat: lat / dailySchedules.length,
