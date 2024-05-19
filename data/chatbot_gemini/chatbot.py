b0c745e1cd26ed63d5c9998b6cfdbe22a826f75e
Nguyễn Thị Phương Thảo <87511103+Phuongthao2230@users.noreply.github.com>
2024-05-19 14:18:21 +0700

    Update file chatbot.py

--- data/chatbot_gemini/chatbot.py
+++ data/chatbot_gemini/chatbot.py
@@ -1,13 +1,14 @@
 import google.generativeai as genai
 
-class GeniAIException(Exception):
-    """GenAI Exception base class"""
+class ChatBotException(Exception):
+    """ChatBot Exception base class"""
 
 class ChatBot:
     """ Chat can only have one candidate count """
     CHATBOT_NAME = 'My Gemini AI'
 
     def __init__(self, api_key):
+        # Initialize the chatbot with the API key and default configuration
         self.genai = genai
         self.genai.configure(api_key=api_key)
         self.model = self.genai.GenerativeModel('gemini-pro')
@@ -15,11 +16,12 @@ class ChatBot:
         self._conversation_history = []
         self.preload_conversation()
 
+    # Send a prompt to the chatbot and get the response
     def send_prompt(self, prompt, temperature=0.1):
         if temperature < 0 or temperature > 1:
-            raise GeniAIException('Temperature must be between 0 and 1')
+            raise ChatBotException('Temperature must be between 0 and 1')
         if not prompt:
-            raise GeniAIException('Prompt cannot be empty')
+            raise ChatBotException('Prompt cannot be empty')
         try:
             response = self.conversation.send_message(
                 content=prompt,
@@ -28,8 +30,9 @@ class ChatBot:
             response.resolve()
             return f'{response.text}\n' + '---' * 20
         except Exception as e:
-            raise GeniAIException(str(e))
+            raise ChatBotException(str(e))
 
+    # Get the conversation history
     @property
     def history(self):
         conversation_history = [
@@ -37,23 +40,28 @@ class ChatBot:
         ]
         return conversation_history
 
+    # Clear the current conversation
     def clear_conversation(self):
         self.conversation = self.model.start_chat(history=[])
 
+    # Start a new conversation
     def start_conversation(self):
         self.conversation = self.model.start_chat(history=self._conversation_history)
 
+    # Configuration for generating responses
     def _generation_config(self, temperature):
         return genai.types.GenerationConfig(
             temperature=temperature
         )
 
+    # Construct a new message
     def _construct_message(self, text, role='user'):
         return {
             'role': role,
             'parts': [text]
         }
 
+    # Preload conversation with the provided history
     def preload_conversation(self, conversation_history=None):
         if isinstance(conversation_history, list):
             self._conversation_history = conversation_history
